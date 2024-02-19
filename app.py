55f33403ab2932951b238484f21c0114919a023d
King-04 <kboateng396@gmail.com>
2024-02-19 22:20:45 +0800

    packages update and database creaion

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from models import db, Jewelry
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jewelry.db'
 db.init_app(app)
+
+@app.route('/')
+def index():
+    # Fetch some example jewelry items from the database
+    jewelry_items = Jewelry.query.all()
+    return render_template('index.html', jewelry_items=jewelry_items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
